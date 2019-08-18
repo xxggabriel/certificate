2c9168ac94e41cec5156de579da3eaaa6c61be62
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-08-18 14:54:51 -0300

    All modified class

--- src/QRCode.php
+++ src/QRCode.php
@@ -2,69 +2,28 @@
 
 namespace Certificate;
 
-use chillerlan\QRCode\QRCode as QR;
-use chillerlan\QRCode\QROptions;
-
 /** 
  * <b>QRCode: </b>This class generate QR code 
  * @author Gabriel Oliveira
  * */
 class QRCode
 {
-    /** @var object $options = Instantiates QROptions class settings */
-    private $options;
-    /** @var string $data = Data to be for generating the QR code */
-    private $data;
 
-    public function __construct($data)
-    {
-
-        $this->options = new QROptions([
-            'version'      => 7,
-            'outputType'   => QR::OUTPUT_IMAGE_PNG,
-            'eccLevel'     => QR::ECC_L,
-            'scale'        => 5,
-            'imageBase64'  => false,
-            'moduleValues' => [
-                // finder
-                1536 => [0, 0, 0], // dark (true)
-                6    => [255, 255, 255], // light (false), white is the transparency color and is enabled by default
-                // alignment
-                2560 => [0, 0, 0],
-                10   => [255, 255, 255],
-                // timing
-                3072 => [0, 0, 0],
-                12   => [255, 255, 255],
-                // format
-                3584 => [0, 0, 0],
-                14   => [255, 255, 255],
-                // version
-                4096 => [0, 0, 0],
-                16   => [255, 255, 255],
-                // data
-                1024 => [0, 0, 0],
-                4    => [255, 255, 255],
-                // darkmodule
-                512  => [0, 0, 0],
-                // separator
-                8    => [255, 255, 255],
-                // quietzone
-                18   => [255, 255, 255],
-            ],
-        ]);
-        $this->data = $data;
-        
-    }
 
     /**
      * <b>Create QR code:<b/> With the past data a QR code is generated.
+     *  @var string $data = QR code data
+     *  @var int $size = Size code data
      *  @return string $tempQrCode = Directory where QR code image was saved
      */
-    public function create()
+    public static function create(string $data, int $size = 150)
     {
-        $qr = new QR($this->options);
+        $qrCodeImage = "https://api.qrserver.com/v1/create-qr-code/?size=".$size."x".$size."&data=".$data;
         $tempQrCode = sys_get_temp_dir().'/'.  md5(uniqid()) . '-' . time() .'QRCode-Certificate-'.rand(0,100).'.png';
-        $qr->render($this->data, $tempQrCode);
+        if(!copy($qrCodeImage, $tempQrCode)){
+            throw new \Exception("Error creating QR code.", 1);
+            
+        }
         return $tempQrCode;
     }
 
